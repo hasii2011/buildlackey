0ffdfd4eebd21d5db4f93c9c6124ceb945c3e90c
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2023-09-13 10:35:03 -0500

    <>[]: <Fix bad build scripts ⚡>
    []
    
    [https://github.com/hasii2011/buildlackey/issues/11]

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -5,7 +5,10 @@ packageMe() {
 clear
 cd scripts || exit
 ./packageme.sh '$*'
+# shellcheck disable=SC2164
+cd ..
 }
+
 
 alias cleanup='clear;   cd scripts;  ./cleanup.sh;   cd -'
 alias runmypy='clear;   cd scripts;  ./runmypy.sh;   cd -'
