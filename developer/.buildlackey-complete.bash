c7a07d84e4bc00ce65a24753caa445967cb87cf1
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2023-11-16 20:48:05 -0600

    <>[]: <Start CLI completion>
    [Save until I figure out how to deliver them]
    
    [https://github.com/hasii2011/buildlackey/issues/21]

---
+++ developer/.buildlackey-complete.bash
@@ -0,0 +1,6 @@
+
+eval "$(_UNITTESTS_COMPLETE=bash_source unittests)"
+eval "$(_RUNMYPY_COMPLETE=bash_source runmypy)"
+eval "$(_CLEANUP_COMPLETE=bash_source cleanup)"
+eval "$(_PACKAGE_COMPLETE=bash_source package)"
+eval "$(_PRODPUSH_COMPLETE=bash_source prodpush)"
